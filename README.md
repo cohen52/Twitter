81b4fb8742c24778c0edfb58294326a36aecceb8
cohen52 <31905089+cohen52@users.noreply.github.com>
2019-02-09 16:52:59 -0500

    One bonus user story completed

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
+# Twitter - Part II
+
+This is a basic twitter app to view, compose, favorite, and retweet tweets.
+
+Time spent: **6** hours spent in total
+
+## User Stories
+
+The following **required** functionality is completed:
+
+- [x] User can compose a tweet. (4pts)
+- [x] User can favorite a tweet. (4pts)
+- [x] User can retweet a tweet. (2pts)
+
+The following **bonus** features are implemented:
+
+- [x] When composing a tweet, user sees a countdown for the number of characters remaining for the tweet (out of 140) (2pts)
+- [ ] User can view their profile in a profile tab. (3pts)
+- [ ] User sees embedded images in tweet if available. (3pts)
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://github.com/cohen52/Twitter/blob/master/twitter_demo.gif' width='' alt='Video Walkthrough' />
+
 # Project 3 - *Twitter*
 
 **Twitter** is a basic twitter app to read your tweets.
@@ -24,4 +50,5 @@ The following **bonus** features are implemented:
 Here's a walkthrough of implemented user stories:
 
 <img src='https://github.com/cohen52/Twitter/blob/master/twitter_demo.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
 
--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,31 +8,59 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var characterCountLabel: UILabel!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
-
+       characterCountLabel.text = "140"
+        tweetTextView.delegate = self
+    
         // Do any additional setup after loading the view.
     }
+    
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func tweet(_ sender: Any) {
+       // characterCountLabel.text = "\(tweetTextView.text.characters.count)"
         if (!tweetTextView.text.isEmpty){
-            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
-                self.dismiss(animated: true, completion: nil)
-            }, failure: { (error) in
-                print("Error posting tweet \(error)")
-                self.dismiss(animated: true, completion: nil)
-            })
+            if(tweetTextView.text.characters.count < 140){
+                TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                    self.dismiss(animated: true, completion: nil)
+                }, failure: { (error) in
+                    print("Error posting tweet \(error)")
+                    self.dismiss(animated: true, completion: nil)
+                })
+            }
         }
     }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        // TODO: Check the proposed new text character count
+        // Allow or disallow the new text
+        // Set the max character limit
+        let characterLimit = 140
+        
+        // Construct what the new text would be if we allowed the user's latest edit
+        
+        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+        
+         characterCountLabel.text = "\(characterLimit - tweetTextView.text.characters.count)"
+        
+        // TODO: Update Character Count Label
+        
+        // The new text should be allowed? True/False
+        return newText.characters.count < characterLimit
+    }
+
     
     /*
     // MARK: - Navigation
